c3751aeb898ec0b4921c651f0dbc4a14a1b9b24d
stfnbssl <stfn.bssl@gmail.com>
2022-06-10 06:21:06 +0200

    dev docs folder, github pages

--- docs/index.js
+++ docs/index.js
@@ -2,6 +2,6 @@
     artifact generator: C:\My\wizzi\stfnbssl\wizzi\packages\wizzi-js\dist\lib\artifacts\js\module\gen\main.js
     package: wizzi-js@0.7.9
     primary source IttfDocument: C:\My\wizzi\stfnbssl\wizzi\docs\.wizzi\ittf\pages\home\index.js.ittf
-    utc time: Thu, 09 Jun 2022 20:26:19 GMT
+    utc time: Fri, 10 Jun 2022 04:20:05 GMT
 */
 'use strict';
